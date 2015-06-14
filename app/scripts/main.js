e094190c061f88d2b761f2801a72ff03008ebab6
Pablo Navarro Castillo <pnavarrc@gmail.com>
2015-06-14 13:05:38 -0300

    Update the radius

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -47,7 +47,7 @@ function earthChart() {
 
       var rScale = d3.scale.sqrt()
         .domain(d3.extent(data.meteorites, function(d) { return d.mass; }))
-        .range([1, 16]);
+        .range([4, 20]);
 
       var cScale = d3.scale.linear()
         .domain(rScale.domain())
